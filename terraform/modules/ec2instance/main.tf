8285e2e17c6f6dcb1a5de9b8f31fd1a22d377089
Klimenko Maxim Sergievich <klimenkomaximsergievich@gmail.com>
2022-03-10 23:41:52 +0200

    Added site deploy

--- terraform/modules/ec2instance/main.tf
+++ terraform/modules/ec2instance/main.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "server" {
 resource "aws_security_group_rule" "server_80" {
   count             = (var.web_server_ingress != false ? 1 : 0 )
   type              = "ingress"
-  from_port         = 0
+  from_port         = 80
   to_port           = 80
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
@@ -43,7 +43,7 @@ resource "aws_security_group_rule" "server_80" {
 resource "aws_security_group_rule" "server_443" {
   count             = (var.web_server_ingress != false ? 1 : 0 )
   type              = "ingress"
-  from_port         = 0
+  from_port         = 443
   to_port           = 443
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
